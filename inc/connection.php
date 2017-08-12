8bf5482b4ec3b59a28e3bb6ad883884a52559c77
Ahmed Salah <GeekAhmeds@Gmail.com>
2017-08-12 21:12:40 +0200

    Add Register Form And Validate If Username And Email >> And Some Filters.

--- inc/connection.php
+++ inc/connection.php
@@ -2,12 +2,11 @@
 try {
 /// To Connect To Database With PDO
    ///
-   $pdo = new PDO('mysql:host=localhost;dbname=NewApp','root','',[
+   $pdo = new PDO('mysql:host=localhost;dbname=newapp;charset=utf8','root','',[
     PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
   ]);
-  $pdo->exec('SET names utf8');
 
 } catch (PDOException $e) {
   print_r($e->getMessage());
